e4373e0d9b5e4614eaf8a948aab94bebaa56c857
mamorucom <eetsutto@gmail.com>
2022-11-01 23:02:09 +0900

    Riverpodで実装したバージョン

--- lib/main.dart
+++ lib/main.dart
@@ -109,17 +109,196 @@
 //   }
 // }
 
+///
+///
+///
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///
+/// Providerを使った複数画面で再描画例
+///
+
+// import 'package:flutter/material.dart';
+// import 'package:provider/provider.dart';
+
+// void main() {
+//   runApp(
+//     MultiProvider(
+//       providers: [
+//         ChangeNotifierProvider<ResultProvider>(
+//           create: (context) => ResultProvider(),
+//         ),
+//       ],
+//       child: MaterialApp(
+//         home: MyHomePage(),
+//       ),
+//     ),
+//   );
+// }
+
+// class ResultProvider extends ChangeNotifier {
+//   String _result;
+
+//   ResultProvider() {
+//     initValue();
+//   }
+
+//   // 初期化
+//   void initValue() {
+//     this._result = "遷移先に移動";
+//   }
+
+//   void refresh() {
+//     initValue();
+//     notifyListeners(); // Providerを介してConsumer配下のWidgetがリビルドされる
+//   }
+
+//   void updateText(String str) {
+//     _result = str;
+//   }
+
+//   void notify() {
+//     notifyListeners(); // Providerを介してConsumer配下のWidgetがリビルドされる
+//   }
+// }
+
+// // 遷移元ページ
+// class MyHomePage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     // テキスト表示
+//     Widget _renderText(ResultProvider model) {
+//       // print('text:${model._result}');
+//       return Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             Text(
+//               model._result, // ResultProviderのプロパティ
+//               style: Theme.of(context).textTheme.headline5,
+//             ),
+//             ElevatedButton(
+//               child: Text('Go to Edit Page'),
+//               onPressed: () async {
+//                 model.updateText('Hello! from HomePage.');
+//                 await Navigator.push(
+//                   context,
+//                   MaterialPageRoute(
+//                     builder: (context) =>
+//                         // 引数に遷移元から遷移先へ渡す値を設定
+//                         EditPage(),
+//                   ),
+//                 );
+//                 // print(result);
+//                 model.notify(); // ResultProviderのメソッド
+//               },
+//             ),
+//           ],
+//         ),
+//       );
+//     }
+
+//     // サンプル１：Scaffold全体をリビルド
+//     // return Consumer<ResultProvider>(builder: (context, model, _) {
+//     //   return Scaffold(
+//     //     appBar: appBar(),
+//     //     body: _renderText(model),
+//     //     floatingActionButton: FloatingActionButton(
+//     //       onPressed: () {
+//     //         model.refresh();
+//     //       },
+//     //       child: Icon(
+//     //         Icons.refresh,
+//     //       ),
+//     //     ),
+//     //   );
+//     // });
+
+//     // サンプル２：appBar以外をリビルド
+//     return Scaffold(
+//       appBar: appBar(),
+//       body: Consumer<ResultProvider>(builder: (context, model, _) {
+//         return _renderText(model);
+//       }),
+//       floatingActionButton:
+//           Consumer<ResultProvider>(builder: (context, model, _) {
+//         return FloatingActionButton(
+//           onPressed: () {
+//             model.refresh(); // ResultProviderのメソッド
+//           },
+//           child: Icon(
+//             Icons.refresh,
+//           ),
+//         );
+//       }),
+//     );
+//   }
+
+//   appBar() {
+//     print('appBar実行');
+//     return AppBar(
+//       title: Text('My Home Page（遷移元）'),
+//     );
+//   }
+// }
+
+// // 遷移先ページ
+// class EditPage extends StatelessWidget {
+//   final receive;
+//   const EditPage({Key key, this.receive}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Consumer<ResultProvider>(
+//       builder: (context, model, child) {
+//         return WillPopScope(
+//           onWillPop: () {
+//             model.updateText('Thank you! from 戻るアイコン'); // ResultProviderのメソッド
+//             Navigator.pop(context);
+//             return Future.value(false);
+//           },
+//           child: Scaffold(
+//             appBar: AppBar(
+//               title: Text('Edit Page（遷移先）'),
+//             ),
+//             body: Center(
+//               child: Column(
+//                 mainAxisAlignment: MainAxisAlignment.center,
+//                 children: [
+//                   Text(
+//                     model._result,
+//                     style: Theme.of(context).textTheme.headline5,
+//                   ),
+//                   ElevatedButton(
+//                       child: Text('Return'),
+//                       onPressed: () {
+//                         model.updateText(
+//                             'Thank you! from 戻るボタン'); // ResultProviderのメソッド
+//                         Navigator.pop(context);
+//                       }),
+//                 ],
+//               ),
+//             ),
+//           ),
+//         );
+//       },
+//     );
+//   }
+// }
+
+///
+///
+///
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///
+/// Providerを使った複数画面で再描画例
+///
+
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void main() {
   runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider<ResultProvider>(
-          create: (context) => ResultProvider(),
-        ),
-      ],
+    ProviderScope(
       child: MaterialApp(
         home: MyHomePage(),
       ),
@@ -127,16 +306,18 @@ void main() {
   );
 }
 
-class ResultProvider extends ChangeNotifier {
-  String _result;
+final resultProvider = ChangeNotifierProvider<ResultProvider>((ref) {
+  return ResultProvider();
+});
 
-  ResultProvider() {
-    initValue();
-  }
+class ResultProvider extends ChangeNotifier {
+  String _result = '遷移先に移動';
+
+  ResultProvider();
 
   // 初期化
   void initValue() {
-    this._result = "遷移先に移動";
+    this._result = '遷移先に移動';
   }
 
   void refresh() {
@@ -208,14 +389,16 @@ class MyHomePage extends StatelessWidget {
     // サンプル２：appBar以外をリビルド
     return Scaffold(
       appBar: appBar(),
-      body: Consumer<ResultProvider>(builder: (context, model, _) {
+      body: Consumer(builder: (context, ref, _) {
+        final model = ref.watch(resultProvider);
         return _renderText(model);
       }),
-      floatingActionButton:
-          Consumer<ResultProvider>(builder: (context, model, _) {
+      floatingActionButton: Consumer(builder: (context, ref, _) {
+        final notifier = ref.watch(resultProvider.notifier);
+
         return FloatingActionButton(
           onPressed: () {
-            model.refresh(); // ResultProviderのメソッド
+            notifier.refresh(); // ResultProviderのメソッド
           },
           child: Icon(
             Icons.refresh,
@@ -236,15 +419,18 @@ class MyHomePage extends StatelessWidget {
 // 遷移先ページ
 class EditPage extends StatelessWidget {
   final receive;
-  const EditPage({Key key, this.receive}) : super(key: key);
+  const EditPage({Key? key, this.receive}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Consumer<ResultProvider>(
-      builder: (context, model, child) {
+    return Consumer(
+      builder: (context, ref, child) {
+        final model = ref.watch(resultProvider);
+        final notifier = ref.watch(resultProvider.notifier);
         return WillPopScope(
           onWillPop: () {
-            model.updateText('Thank you! from 戻るアイコン'); // ResultProviderのメソッド
+            notifier
+                .updateText('Thank you! from 戻るアイコン'); // ResultProviderのメソッド
             Navigator.pop(context);
             return Future.value(false);
           },
@@ -263,7 +449,7 @@ class EditPage extends StatelessWidget {
                   ElevatedButton(
                       child: Text('Return'),
                       onPressed: () {
-                        model.updateText(
+                        notifier.updateText(
                             'Thank you! from 戻るボタン'); // ResultProviderのメソッド
                         Navigator.pop(context);
                       }),
